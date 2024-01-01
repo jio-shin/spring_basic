2dd0f87a333c93f3d5477bc5ed3eb1ccbab1ceca
jio-shin <rosieoh99@gmail.com>
2024-01-01 23:23:14 +0900

    second upload

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -19,6 +22,8 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+
+    //회원 등록
     //get은 조회할 때 쓰이는 것
     @GetMapping("/members/new")
     public String createForm() {
@@ -36,5 +41,16 @@ public class MemberController {
         memberService.join(member);
         //회원가입이 끝나면 홈으로 돌려버리는 것
         return "redirect:/";
+    }
+
+    //회원 조회
+    //서버 다시 키면 내용 다 날라감-> db에 저장해야 한다!
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        //model 에 members리스트 담아서 넘김
+
+        return "members/memberList.html";
     }
 }
